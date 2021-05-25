058286853e89eeaa18b053b296a09a5146f5cb4d
RobbenderTT <robert@saastransformations.com>
2021-05-24 17:47:50 -0700

    Added QuestionBox comp

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="question-box-container mt-5">
+        <!-- <b-container> -->
+             <b-jumbotron class="bg-secondary text-white p-4">
+
+            <template #lead>
+            This is a simple hero unit, a simple jumbotron-style component for calling extra attention to
+            featured content or information.
+            </template>
+
+            <hr class="my-4">
+
+            <p>
+            It uses utility classes for typography and spacing to space content out within the larger
+            container.
+            </p>
+
+            <b-button variant="primary" href="#">Do Something</b-button>
+            <b-button variant="success" href="#">Do Something Else</b-button>
+        </b-jumbotron>
+        <!-- </b-container> -->
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+.container .jumbotron {
+    border-radius: 6px ;
+}
+</style>
